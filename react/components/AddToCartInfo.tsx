c445a03ffab320a66cb8b75710db79c4641f1a25
Michael <maicol_a_18@hotmail.com>
2022-12-20 09:50:07 -0500

    feat: add styles in modal

--- react/components/AddToCartInfo.tsx
+++ react/components/AddToCartInfo.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useProduct } from 'vtex.product-context';
+//import { useProduct } from 'vtex.product-context';
 import { useOrderForm } from 'vtex.order-manager/OrderForm';
 import ButtonGroup from "./ButtonGroup";
 import Totalizers from "./Totalizers";
@@ -8,20 +8,18 @@ import { generateBlockClass } from '@vtex/css-handles'
 import styles from './styles.css'
 
 const AddToCartInfo = ({blockClass}: {blockClass: string} ) => {
-  const productInfo = useProduct();
+  //const productInfo = useProduct();
   const { orderForm: {
     items,
     totalizers
   } } = useOrderForm();
   const container = generateBlockClass(styles.container, blockClass);
-
-  console.log("Este producto tiene esta info", productInfo);
   console.log("Esta es mi info de mi orden", totalizers[0]);
   return (
     <div className={container}>
       <ProductGroup products={items} blockClass={blockClass}/>
       <Totalizers total={totalizers[0]} items={items}/>
-      <ButtonGroup />
+      <ButtonGroup blockClass={blockClass}/>
     </div>
   )
 }
